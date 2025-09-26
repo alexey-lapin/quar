88f970acff5f1dc32c9b71f4fa112b8cbed69195
Alexey Lapin <alexey-lapin@protonmail.com>
2025-09-25 21:51:23 -0400

    add gha

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import fs from 'fs'
 import path from 'path'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    host: '0.0.0.0',
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem'))
+    base: process.env.VITE_APP_PUBLIC_PATH ?? "/",
+    plugins: [vue()],
+    server: {
+        port: 3000,
+        host: '0.0.0.0',
+        https: {
+            key: fs.readFileSync(path.resolve(__dirname, 'certs/key.pem')),
+            cert: fs.readFileSync(path.resolve(__dirname, 'certs/cert.pem'))
+        }
     }
-  }
 })
